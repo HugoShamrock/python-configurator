4d07955df7c2bc1b46a9b19eeae619aec6457336
Hugo Shamrock <hugo.shamrock@gmail.com>
2016-11-22 11:23:16 +0100

    mod package setup

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+
+import configurator
+
+long_description = open('README.rst').read() + '\n' + open('CHANGES.rst').read()
 
 setup(
     name='python-configurator',
-    version='0.3',
+    version=configurator.__version__,
     description='Python module for reading configuration files in different formats',
-    long_description=open('README.rst').read() + '\n' + open('CHANGES.rst').read(),
+    long_description=long_description,
     author='Hugo Shamrock',
     author_email='hugo.shamrock@gmail.com',
     url='https://github.com/HugoShamrock/python-configurator',
